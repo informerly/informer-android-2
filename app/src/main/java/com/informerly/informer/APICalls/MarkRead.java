8d4ce37c7be4931472f99db0a811c9b096813b8b
Matias <matias@automative.io>
2015-08-08 13:36:31 -0300

    Using secure http on Read Article Service.

--- app/src/main/java/com/informerly/informer/APICalls/MarkRead.java
+++ app/src/main/java/com/informerly/informer/APICalls/MarkRead.java
@@ -7,19 +7,19 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
-
+import org.apache.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MarkRead {
-    String token, id,articleid;
+    String token, id, articleid;
     static String baseurl ;
     public MarkRead(String tooken, String userid, String feedid) {
         this.token = tooken;
         this.id = userid;
         this.articleid = feedid;
-        baseurl = "http://informerly.com/api/v1/links/"+articleid+"/read";
+        baseurl = "https://informerly.com/api/v1/links/"+articleid+"/read";
     }
 
     public void mark() {
@@ -29,12 +29,12 @@ public class MarkRead {
             HttpPost post = new HttpPost(baseurl);
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair("auth_token",token));
-            params.add(new BasicNameValuePair("client_id",id));
+            params.add(new BasicNameValuePair("client_id", id));
             UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
             post.setEntity(ent);
-            client.execute(post);
+            HttpResponse response = client.execute(post);
+
         } catch (Exception e) {
         }
     }
 }
-
