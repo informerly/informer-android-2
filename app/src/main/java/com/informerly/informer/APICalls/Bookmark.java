4862dfe874f9c0fa3f6c96af468b12a8209fa076
Matias <matias@automative.io>
2015-08-24 12:31:48 -0300

    Introducing new Bookmark api call class.

---
+++ app/src/main/java/com/informerly/informer/APICalls/Bookmark.java
@@ -0,0 +1,48 @@
+package com.informerly.informer.APICalls;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.informerly.informer.FeedView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpResponse;
+
+public class Bookmark {
+    String token, id, articleid;
+    static String baseurl ;
+    HttpEntity resEntity = null;
+    SharedPreferences sharedpreferences;
+
+    public Bookmark(String feedid, Boolean bookmarked) {
+
+        sharedpreferences = FeedView.getContext().getSharedPreferences("user_session", Context.MODE_PRIVATE);
+        this.token = sharedpreferences.getString("token", null);;
+        this.id = sharedpreferences.getString("userid", null);;
+        this.articleid = feedid;
+
+        if(bookmarked) {
+            baseurl = "https://informerly.com/api/v1/bookmarks?auth_token=" + token + "&client_id=" + id;
+        } else {
+            baseurl = "https://informerly.com/api/v1/unbookmarks?auth_token=" + token + "&client_id=" + id;
+        }
+    }
+
+    public HttpEntity bookmark() {
+        try {
+            HttpClient client = new DefaultHttpClient();
+            client.getParams().setParameter(org.apache.http.params.CoreProtocolPNames.USER_AGENT, System.getProperty("http.agent"));
+            HttpPost post = new HttpPost(baseurl);
+            HttpResponse response = client.execute(post);
+            resEntity = response.getEntity();
+            return resEntity;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
